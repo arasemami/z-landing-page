b68e5e6ad57679cd8afb55c7e373b4ab516e9717
aras armani <aras.armani2014@gmail.com>
2020-02-12 21:33:10 +0300

    first commit

---
+++ src/app/components/index/top-screen/top-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-screen',
+  templateUrl: './top-screen.component.html',
+  styleUrls: ['./top-screen.component.scss']
+})
+export class TopScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
