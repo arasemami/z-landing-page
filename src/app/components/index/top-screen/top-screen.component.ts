f971d83c7535a7e82ab715618c19e683565acbd9
Aras <aras.armani2014@gmail.com>
2020-02-18 13:04:12 +0330

    video player is added

--- src/app/components/index/top-screen/top-screen.component.ts
+++ src/app/components/index/top-screen/top-screen.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopScreenComponent implements OnInit {
 
+  isVideoActive = false;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  deletePhone(val) {
+    this.isVideoActive = val;
+  }
+
+
 
 }
--- src/app/components/index/top-screen/video-player/video-player.component.ts
+++ src/app/components/index/top-screen/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-video-player',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoPlayerComponent implements OnInit {
 
+  @Output() statusEvent = new EventEmitter<boolean>();
+
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+
+  callParent() {
+    this.statusEvent.next(false);
   }
 
 }
