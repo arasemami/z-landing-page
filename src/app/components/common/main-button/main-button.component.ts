a3c76cb90826fdaa08be99e5186f606f342538eb
Aras <aras.armani2014@gmail.com>
2020-02-18 13:23:29 +0330

    footer is added

--- src/app/components/common/main-button/main-button.component.ts
+++ src/app/components/common/main-button/main-button.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class MainButtonComponent implements OnInit {
 
   @Input() text: string;
+  @Input() color: string;
 
   constructor() { }
 
